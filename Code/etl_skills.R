74365a1b324f51fc7cabf96eabfa3c9f1632eec5
Cristian Sarabia <cristiansarabia94@gmail.com>
2025-04-18 08:27:02 -0600

    Updated git ignore, and updated the location for one drive

--- Code/etl_skills.R
+++ Code/etl_skills.R
@@ -1,19 +1,19 @@
 # Skills Study
 # Data Dictionary
-
+``
 rm(list = ls())
 
 library(tidyverse)
 library(readxl)
 library(writexl)
 
-root_wd = "C:/Users/sarabiac"
-onedrive_wd = file.path(root_wd, "OneDrive - The University of Colorado Denver/College of Nursing/Skills Study/Bowler, Fara's files - March 2023_FB BH SH")
-github_wd = file.path(root_wd, "OneDrive - The University of Colorado Denver/College of Nursing/Repos/Windows Repo/Bowler-231464")
+root_wd = "/Users/cristiansarabia/Library/CloudStorage"
+onedrive_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Skills Study/Bowler, Fara's files - March 2023_FB BH SH")
+github_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Repos/Mac Repo/Bowler-231464")
 
 source(file.path(github_wd, "Code", "utils", "utils.R"))
 
-setwd(github_wd)
+#setwd(github_wd)
 
 # Construct data dictionary
 dict = construct_data_dictionary(onedrive_wd=onedrive_wd)
