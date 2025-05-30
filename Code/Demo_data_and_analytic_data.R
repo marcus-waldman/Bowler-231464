a5e89d14088bbed75bd86dd899342617cd047326
marcus-waldman <marcus.waldman@cuanschutz.edu>
2025-05-30 16:21:18 -0600

    analytic dataset code. needs further invest.

--- Code/Demo_data_and_analytic_data.R
+++ Code/Demo_data_and_analytic_data.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 # Skills Study
 # Creating Analytic Data Set
 
@@ -11,17 +13,22 @@ library(tidyverse)
 #github_wd = file.path(root_wd, "OneDrive - The University of Colorado Denver/College of Nursing/Repos/Windows Repo/Bowler-231464")
 
 # Cristian's Mac
-root_wd = "/Users/cristiansarabia/Library/CloudStorage"
-onedrive_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Skills Study/Bowler, Fara's files - March 2023_FB BH SH")
-github_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Repos/Mac Repo/Bowler-231464")
+#root_wd = "/Users/cristiansarabia/Library/CloudStorage"
+#onedrive_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Skills Study/Bowler, Fara's files - March 2023_FB BH SH")
+#github_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Repos/Mac Repo/Bowler-231464")
 
 
 # Marcus W. Locations
-# root_wd =
-# onedrive_wd = 
-# github_wd =
+root_wd = "C:/Users/waldmanm/"
+onedrive_wd = file.path(root_wd,"The University of Colorado Denver", "Bowler, Fara - March 2023_FB BH SH")
+github_wd = file.path(root_wd,"git-repositories", "Bowler-231464")
 
-source(file.path(github_wd, "Code", "participant_demographics_data.R"))
+#source(file.path(github_wd, "Code", "participant_demographics_data.R"))
+source(file.path(github_wd, "Code", "utils", "utils.R"))
 
-demo_data = demo_graphics_data(onedrive_wd=onedrive_wd)
+
+# Load in analytic dataset
+dat = demo_and_response_data(onedrive_wd = onedrive_wd)
+
+
 
