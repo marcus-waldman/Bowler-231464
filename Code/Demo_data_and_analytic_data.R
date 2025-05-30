624a355fa794f6bcef5baf2849b09a43cde40404
Cristian Sarabia <cristiansarabia94@gmail.com>
2025-05-30 09:02:40 -0600

    Finishing up script to bring in demographics data.

--- Code/Demo_data_and_analytic_data.R
+++ Code/Demo_data_and_analytic_data.R
@@ -1,7 +1,8 @@
 # Skills Study
-# Contingency Tables
+# Creating Analytic Data Set
 
 library(tidyverse)
+
 
 
 # Cristian'S Windows
@@ -13,3 +14,14 @@ library(tidyverse)
 root_wd = "/Users/cristiansarabia/Library/CloudStorage"
 onedrive_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Skills Study/Bowler, Fara's files - March 2023_FB BH SH")
 github_wd = file.path(root_wd, "OneDrive-TheUniversityofColoradoDenver/College of Nursing/Repos/Mac Repo/Bowler-231464")
+
+
+# Marcus W. Locations
+# root_wd =
+# onedrive_wd = 
+# github_wd =
+
+source(file.path(github_wd, "Code", "participant_demographics_data.R"))
+
+demo_data = demo_graphics_data(onedrive_wd=onedrive_wd)
+
