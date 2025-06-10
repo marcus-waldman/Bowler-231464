b0f0a54b3b814d070a59a49be63d3cce929e886d
marcus-waldman <marcus.waldman@cuanschutz.edu>
2025-06-10 17:27:54 -0600

    coefficient estimates

--- Code/Mixed-effect-models2.R
+++ Code/Mixed-effect-models2.R
@@ -9,6 +9,10 @@ library(lmtest)
 library(future)
 library(future.apply)
 
+library(sjPlot)
+library(sjmisc)
+library(sjlabelled)
+
 plan(strategy="multisession", workers = 8)
 
 # Cristian'S Windows
@@ -23,39 +27,145 @@ plan(strategy="multisession", workers = 8)
 
 
 # Marcus W. Locations
-#root_wd = "C:/Users/waldmanm/"
-#onedrive_wd = file.path(root_wd,"OneDrive - The University of Colorado Denver", "Bowler, Fara's files - March 2023_FB BH SH")
-#github_wd = file.path(root_wd,"git-repositories", "Bowler-231464")
-
-# Marcus's Home Desktop (White-Rhino) 
-root_wd = "C:/Users/marcu"
+root_wd = "C:/Users/waldmanm/"
 onedrive_wd = file.path(root_wd,"OneDrive - The University of Colorado Denver", "Bowler, Fara's files - March 2023_FB BH SH")
 github_wd = file.path(root_wd,"git-repositories", "Bowler-231464")
+
+# Marcus's Home Desktop (White-Rhino) 
+#root_wd = "C:/Users/marcu"
+#onedrive_wd = file.path(root_wd,"OneDrive - The University of Colorado Denver", "Bowler, Fara's files - March 2023_FB BH SH")
+#github_wd = file.path(root_wd,"git-repositories", "Bowler-231464")
 
 
 #source(file.path(github_wd, "Code", "participant_demographics_data.R"))
 source(file.path(github_wd, "Code", "utils", "utils.R"))
 
-
 # Load in analytic dataset
 dat = demo_and_response_data(onedrive_wd = onedrive_wd) 
 
 # Restrict to round 1 only
-dat = dat %>% dplyr::filter(round==1) 
+dat = dat %>% dplyr::filter(round==1) %>% 
+  dplyr::mutate(rid = 1:n()) %>% 
+  dplyr::relocate(rid)
+
+# Let's recode the expertise & dummies
+expertise_df = dat %>% 
+  dplyr::select(rid, expertise) %>% 
+  dplyr::filter(!is.na(expertise)) %>% 
+  fastDummies::dummy_cols("expertise", split = ",")
+
+# setting_df = dat %>% 
+#   dplyr::select(name, setting) %>% 
+#   dplyr::filter(!is.na(setting)) %>% 
+#   fastDummies::dummy_cols("setting", split = ",")
+
+# Attach expertise data
+dat = dat %>% 
+  dplyr::left_join(expertise_df, by = "rid")
 
 # Cannot get region model to converge
-groupings_vec = c("role_primary","gender","age_years",
-                  "experience_edu_years","experience_rn_years") 
+groupings_vec = c(
+  "role_primary",
+  "gender",
+  "age_years",
+  "experience_edu_years",
+  "experience_rn_years"
+) 
+
 
 J = length(groupings_vec)
-out_list<-future.apply::future_lapply(1:J, function(j){
+for(j in 1:J){
+  
   x = groupings_vec[j]
   print(x)
   dat_x = dat %>%
-    dplyr::select(essential, category, name, varshort, dplyr::any_of(x)) %>%
-    dplyr::rename(grouping = x) %>%
-    na.omit()
+    dplyr::select("essential", "category", "name", "varshort", dplyr::any_of(groupings_vec)) %>%
+    dplyr::rename(grouping = x) 
+  if(is.ordered(dat_x$grouping)){
+    dat_x$grouping = as.integer(dat_x$grouping)
+    
+    median_val = dat_x %>%
+      dplyr::group_by(name) %>%
+      dplyr::reframe(grouping = grouping[1]) %>%
+      purrr::pluck("grouping") %>%
+      median(na.rm = T)
+    
+    dat_x = dat_x %>%
+      dplyr::mutate(grouping = (grouping>median_val))
+    
+  }
+  if(is.factor(dat_x$grouping) | is.ordered(dat_x$grouping)){
+    contrasts(dat_x$grouping) = "contr.sum"
+  }
+  
+  
+  fit0 = lme4::glmer(
+    essential~category+(1|name) + (1|varshort), 
+    data = dat_x %>% dplyr::select(essential,category,name,varshort) %>% na.omit(), 
+    family = "binomial", 
+    verbose = 1, 
+    control = glmerControl(optimizer = "bobyqa")
+  )
+  filepath =  paste0(x, " Model 0.html")
+  sjPlot::tab_model(fit0, title = paste0(x, " Model 0.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+  
+  fit1 = lme4::glmer(
+    essential~category+grouping+(1|name) + (1|varshort), 
+    data = dat_x %>% dplyr::select(essential,category,grouping,name,varshort) %>% na.omit(), 
+    family = "binomial", 
+    verbose = 1, 
+    control = glmerControl(optimizer = "bobyqa")
+  )
+  filepath =  paste0(x, " Model 1.html")
+  sjPlot::tab_model(fit1, title = paste0(x, " Model 1.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+  
+  
+  fit2 = lme4::glmer(
+    essential~category+grouping+category*grouping+(1|name) + (1|varshort), 
+    data = dat_x %>% dplyr::select(essential,category,grouping,name,varshort) %>% na.omit(), 
+    family = "binomial", 
+    verbose = 1, 
+    control = glmerControl(optimizer = "bobyqa")
+  )
+  
+  filepath =  paste0(x, " Model 2.html")
+  sjPlot::tab_model(fit2, title = paste0(x, " Model 2.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+  
+  if(x == "role_primary"){
+    
+    fit3 = lme4::glmer(
+      essential~category*grouping*experience_rn_years+(1|name) + (1|varshort), 
+      data = dat_x %>% dplyr::select(essential,category,grouping,name,varshort,experience_rn_years) %>% na.omit(), 
+      family = "binomial", 
+      verbose = 1, 
+      control = glmerControl(optimizer = "bobyqa")
+    )
+    filepath =  paste0(x, " Model 3.html")
+    sjPlot::tab_model(fit3, title = paste0(x, " Model 3.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+    
+    
+    fit4 = lme4::glmer(
+      essential~category*grouping*experience_rn_years*age_years+(1|name) + (1|varshort), 
+      data = dat_x %>% dplyr::select(essential,category,grouping,name,varshort,experience_rn_years,age_years) %>% na.omit(), 
+      family = "binomial", 
+      verbose = 1, 
+      control = glmerControl(optimizer = "bobyqa")
+    )
+    filepath =  paste0(x, " Model 4.html")
+    sjPlot::tab_model(fit4, title = paste0(x, " Model 4.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+    
+  }
+  
+  
+}
 
+out_list<-future.apply::future_lapply(1:J, function(j){
+
+  x = groupings_vec[j]
+  print(x)
+  dat_x = dat %>%
+    dplyr::select("essential", "category", "name", "varshort", dplyr::any_of(groupings_vec)) %>%
+    dplyr::rename(grouping = x) 
   if(is.ordered(dat_x$grouping)){
     dat_x$grouping = as.integer(dat_x$grouping)
 
@@ -73,13 +183,66 @@ out_list<-future.apply::future_lapply(1:J, function(j){
     contrasts(dat_x$grouping) = "contr.sum"
   }
 
+  
+  fit0 = lme4::glmer(
+    essential~category+(1|name) + (1|varshort), 
+    data = dat_x %>% dplyr::select(essential,category,name,varshort) %>% na.omit(), 
+    family = "binomial", 
+    verbose = 1, 
+    control = glmerControl(optimizer = "bobyqa")
+  )
+  
+  filepath =  paste0(x, " Model 0.html")
+  sjPlot::tab_model(fit0, title = paste0(x, " Model 0.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+  
+  fit1 = lme4::glmer(
+    essential~category+grouping+(1|name) + (1|varshort), 
+    data = dat_x %>% dplyr::select(essential,category,grouping,name,varshort) %>% na.omit(), 
+    family = "binomial", 
+    verbose = 1, 
+    control = glmerControl(optimizer = "bobyqa")
+  )
+  
+  filepath =  paste0(x, " Model 1.html")
+  sjPlot::tab_model(fit1, title = paste0(x, " Model 0.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+  
 
-  fit0 = lme4::glmer(essential~category+(1|name) + (1|varshort), data = dat_x, family = "binomial", verbose = 1, control = glmerControl(optimizer = "bobyqa"))
-  fit1 = lme4::glmer(essential~category+grouping+(1|name) + (1|varshort), data = dat_x, family = "binomial", verbose = 1, control = glmerControl(optimizer = "bobyqa"))
-  fit2 = lme4::glmer(essential~category+grouping+category*grouping+(1|name) + (1|varshort), data = dat_x, family = "binomial", verbose = 1, control = glmerControl(optimizer = "bobyqa"))
+  fit2 = lme4::glmer(
+    essential~category+grouping+category*grouping+(1|name) + (1|varshort), 
+    data = dat_x %>% dplyr::select(essential,category,grouping,name,varshort) %>% na.omit(), 
+    family = "binomial", 
+    verbose = 1, 
+    control = glmerControl(optimizer = "bobyqa")
+  )
+  
+  filepath =  paste0(x, " Model 2.html")
+  sjPlot::tab_model(fit1, title = paste0(x, " Model 2.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+  
   if(x == "role_primary"){
-    fit3 = lme4::glmer(essential~category*grouping*experience_rn_years+(1|name) + (1|varshort), data = dat_x, family = "binomial", verbose = 1, control = glmerControl(optimizer = "bobyqa"))
-    fit4 = lme4::glmer(essential~category*grouping*experience_rn_years*age_years+(1|name) + (1|varshort), data = dat_x, family = "binomial", verbose = 1, control = glmerControl(optimizer = "bobyqa"))
+    
+    fit3 = lme4::glmer(
+      essential~category*grouping*experience_rn_years+(1|name) + (1|varshort), 
+      data = dat_x %>% dplyr::select(essential,category,grouping,experience_rn_years) %>% na.omit(), 
+      family = "binomial", 
+      verbose = 1, 
+      control = glmerControl(optimizer = "bobyqa")
+    )
+    
+    
+    filepath =  paste0(x, " Model 3.html")
+    sjPlot::tab_model(fit1, title = paste0(x, " Model 3.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+    
+    fit4 = lme4::glmer(
+      essential~category*grouping*experience_rn_years*age_years+(1|name) + (1|varshort), 
+      data = dat_x %>% dplyr::select(essential,category,grouping,experience_rn_years,age_years) %>% na.omit(), 
+      family = "binomial", 
+      verbose = 1, 
+      control = glmerControl(optimizer = "bobyqa")
+    )
+    
+    filepath =  paste0(x, " Model 4.html")
+    sjPlot::tab_model(fit1, title = paste0(x, " Model 4.html"), file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, filepath))
+    
   }
 
   lrt_01 = lmtest::lrtest(fit0,fit1) %>%
@@ -91,7 +254,7 @@ out_list<-future.apply::future_lapply(1:J, function(j){
     data.frame() %>%
     dplyr::slice(-1) %>%
     dplyr::mutate(Comparison = "M0 vs. M2", Grouping = x)
-                    
+
   lrt_12 = lmtest::lrtest(fit1,fit2) %>%
     data.frame() %>%
     dplyr::slice(-1) %>%
@@ -99,37 +262,32 @@ out_list<-future.apply::future_lapply(1:J, function(j){
 
 
   if(x == "role_primary"){
-    
+
     lrt_03 = lmtest::lrtest(fit0,fit3) %>%
       data.frame() %>%
       dplyr::slice(-1) %>%
       dplyr::mutate(Comparison = "M0 vs. M3", Grouping = x)
-    
+
     lrt_04 = lmtest::lrtest(fit0,fit4) %>%
       data.frame() %>%
       dplyr::slice(-1) %>%
       dplyr::mutate(Comparison = "M0 vs. M4", Grouping = x)
-    
-    lrt_12 = lmtest::lrtest(fit1,fit2) %>%
-      data.frame() %>%
-      dplyr::slice(-1) %>%
-      dplyr::mutate(Comparison = "M1 vs. M2", Grouping = x)
-    
+
     lrt_13 = lmtest::lrtest(fit1,fit3) %>%
       data.frame() %>%
       dplyr::slice(-1) %>%
       dplyr::mutate(Comparison = "M1 vs. M3", Grouping = x)
-    
+
     lrt_14 = lmtest::lrtest(fit1,fit4) %>%
       data.frame() %>%
       dplyr::slice(-1) %>%
       dplyr::mutate(Comparison = "M1 vs. M4", Grouping = x)
-    
+
     lrt_23 = lmtest::lrtest(fit2,fit3) %>%
       data.frame() %>%
       dplyr::slice(-1) %>%
       dplyr::mutate(Comparison = "M2 vs. M3", Grouping = x)
-    
+
     lrt_24 = lmtest::lrtest(fit1,fit4) %>%
       data.frame() %>%
       dplyr::slice(-1) %>%
@@ -143,8 +301,8 @@ out_list<-future.apply::future_lapply(1:J, function(j){
   }
 
   lrts_x = lrt_01 %>%
-    dplyr::bind_rows(lrt_02) %>% 
-    dplyr::bind_rows(lrt_12) 
+    dplyr::bind_rows(lrt_02) %>%
+    dplyr::bind_rows(lrt_12)
 
   if(x == "role_primary"){
      lrts_x = lrts_x %>%
@@ -157,17 +315,30 @@ out_list<-future.apply::future_lapply(1:J, function(j){
        dplyr::bind_rows(lrt_34)
   }
 
-  lrts_x = lrts_x %>%
-    dplyr::mutate(pvalue.bonferroni = J*Pr..Chisq., N = nrow(dat_x), missing_data_strategy = "Complete Case Analysis") %>%
-    dplyr::select(Grouping,Comparison, missing_data_strategy, N, LogLik, Df, Chisq, pvalue.bonferroni) %>%
-    dplyr::mutate(stars = ifelse(pvalue.bonferroni<.05, "*","")) %>%
-    dplyr::mutate(pvalue.bonferroni = ifelse(pvalue.bonferroni>1, 1, pvalue.bonferroni))
+  fits = list(fit0=fit0,fit1=fit1,fit2=fit2)
+  if(x == "role_primary"){
+    fits$fit3 = fit3;
+    fits$fit4 = fit4;
+  }
 
-  return(list(lrts = lrts_x, fits = list(fit0=fit0,fit1=fit1,fit2=fit2)))
+  # lrts_x = lrts_x %>%
+  #   dplyr::mutate(pvalue.unadjusted = Pr..Chisq., N = nrow(dat_x), missing_data_strategy = "Complete Case Analysis") %>%
+  #   dplyr::select(Grouping,Comparison, missing_data_strategy, N, LogLik, Df, Chisq, pvalue.unadjusted) %>%
+  #   dplyr::mutate(stars = ifelse(pvalue.unadjusted<.05, "*","")) 
+
+  #writexl::write_xlsx(lrts_x, path = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up", "coefficients", x, paste0(x, " LRTs.xlsx")))
+  
+  
+  #return(list(lrts = lrts_x, fits = fits))
+  return(NULL)
 })
 names(out_list) = groupings_vec
-readr::write_rds(out_list, file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up","mixed_models.rds"))
-out_list = readr::read_rds(file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up","mixed_models.rds"))
+#readr::write_rds(out_list, file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up","mixed_models.rds"), compress = "gz")
+#out_list = readr::read_rds(file = file.path(onedrive_wd, "Meeting Memos", "2025-06-04 Follow-up","mixed_models.rds"))
+
+
+
+
 
 out_df = lapply(1:J, function(j){
   lrts_j = out_list[[j]]$lrts
